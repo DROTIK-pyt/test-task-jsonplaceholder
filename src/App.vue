eed4d728e71effa85bd96bd82b06da11e378e329
IVAN <ivan.spiridonov.01@inbox.ru>
2023-08-31 15:33:13 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4 offset-sm-4 col-12">
+          <form @submit.prevent="emptyEvent" action="">
+            <div class="form-group">
+              <input
+                type="text"
+                autocomplete="off"
+                @change="search"
+                v-model="userNameSearch"
+                class="form-control"
+                placeholder="Filter by author.."
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row">
+        <div
+          v-show="!isEmptySearch"
+          v-for="post in posts"
+          :key="post.id"
+          class="col-sm-4 col-12"
+        >
+          <CardPost
+            :title="post.title"
+            :body="post.body"
+            :userName="authorName(post.userId)"
+          />
+        </div>
+        <div class="col-12" v-show="isEmptySearch">Posts not found.</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CardPost from "./components/CardPost.vue";
 
 export default {
-  name: "App",
+  name: "Test-task",
+  data: function () {
+    return {
+      posts: [],
+      users: [],
+      allPosts: [],
+      userNameSearch: "",
+      isEmptySearch: false,
+    };
+  },
   components: {
-    HelloWorld,
+    CardPost,
+  },
+  methods: {
+    authorName(userId) {
+      return (
+        this.users.find((user) => userId == user.id)?.name ||
+        "Author not found."
+      );
+    },
+    search() {
+      let usersFound = [];
+      if (this.userNameSearch.length > 1) {
+        this.users.forEach((user) => {
+          if (user.name == this.userNameSearch) {
+            usersFound.push(user.id);
+          }
+        });
+        this.posts = this.posts.filter((post) =>
+          usersFound.includes(post.userId)
+        );
+      } else {
+        this.posts = this.allPosts;
+      }
+      this.isEmptySearch = this.posts.length == 0;
+    },
+    emptyEvent() {},
+  },
+  beforeMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        this.users = users;
+      });
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        this.posts = posts;
+        this.allPosts = posts;
+      });
   },
 };
 </script>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/CardPost.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <h3 class="card__post-title text-primary">{{ title }}</h3>
+    <p class="card__post-body">{{ body }}</p>
+    <p class="card__author text-muted">{{ userName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardPost",
+  props: {
+    title: String,
+    body: String,
+    userName: String,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.card {
+  width: 100%;
+  padding: 10px 15px;
+  margin-bottom: 30px;
+
+  &__post-title {
+    height: 68px;
+    overflow: hidden;
+  }
+
+  &__post-body {
+    height: 75px;
+    overflow: hidden;
+  }
+}
+</style>
